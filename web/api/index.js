6989c3e4cf5233313f1b177b43692572dd1efb60
灵感仰 <44220593+1239937890@users.noreply.github.com>
2020-09-08 17:37:36 +0800

    1

---
+++ web/api/index.js
@@ -0,0 +1,16 @@
+
+// let http = {
+//   getShort: params => {
+//     return axios({
+//       methods: 'GET',
+//       url: 'http://192.168.100.92/api/sendSms',
+//       params,
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//       }
+//     })
+//   },
+// }
+// export{
+//   http 
+// }
